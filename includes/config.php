8f2abdd6e1c24f04f867abef97f3749edc3d749a
yoannclaisse <claisse.yoann@gmail.com>
2022-06-21 13:13:09 +0200

    first_commit

---
+++ includes/config.php
@@ -0,0 +1,21 @@
+<?php
+// Errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Database
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_NAME', 'esin_p2025_first');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+$pdo = new PDO(
+    'mysql:host='.DB_HOST.';dbname='.DB_NAME.';port='.DB_PORT,
+    DB_USER,
+    DB_PASS
+);
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    // include_once './includes/config.php';
+    include './includes/header.php';
+    echo '<pre>';
+    print_r('php run');
+    echo '</pre>';
+?>
+
+<!-- include footer -->
+<?php include './includes/footer.php';?>
